ce7a52584b08b5d7ec38a716652ee4e3711964e2
Tristan Thomas <tristant@adobe.com>
2025-12-16 16:53:44 +0100

    feat: restliche MUI-Komponenten zum Ausgaben-Form hinzugefügt

--- dontgobroke-frontend/src/app/expenseform/expenseform.ts
+++ dontgobroke-frontend/src/app/expenseform/expenseform.ts
@@ -10,7 +10,13 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatFormFieldModule, MatFormFieldControl } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+
+// interface to represent the form data that is passed to the dialog
 
 export interface ExpenseformData {
   title: string;
@@ -26,12 +32,18 @@ export interface ExpenseformData {
     MatDialogActions,
     MatDialogContent,
     MatDialogTitle,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule
   ],
   templateUrl: './expenseform.html',
   styleUrl: './expenseform.css',
+  providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
+// injection of form data from the dialog
 
 export class Expenseform {
   readonly dialogRef = inject(MatDialogRef<Expenseform>);
